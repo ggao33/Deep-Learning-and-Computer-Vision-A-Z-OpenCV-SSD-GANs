17a431ff886e4ece1f344deedad5c43b23fcf9ab
ggao33 <ggao33@gatech.edu>
2020-06-29 11:49:47 +0800

    Module_1_Homework

---
+++ Module_1_Homework/catface_detector.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 16:37:19 2020
+
+@author: gg
+"""
+
+# Smile Recognition
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface.xml') # We load the cascade for the face.
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+
+def detect(gray, frame): # We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
+    for (x, y, w, h) in faces: # For each detected face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
+        roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+
+    return frame # We return the image with the detector rectangles.
+
+video_capture = cv2.VideoCapture(0) # We turn the webcam on. 0 is pc cam, 1 is external cam
+
+while True: # We repeat infinitely (until break):
+    _, frame = video_capture.read() # We get the last frame.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # We do some colour transformations.
+    canvas = detect(gray, frame) # We get the output of our detect function.
+    cv2.imshow('Video', canvas) # We display the outputs.
+    if cv2.waitKey(1) & 0xFF == ord('q'): # If we type on the keyboard:
+        break # We stop the loop.
+
+video_capture.release() # We turn the webcam off.
+cv2.destroyAllWindows() # We destroy all the windows inside which the images were displayed.
